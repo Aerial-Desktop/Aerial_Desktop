0e5d320ae6e6887001b8dd65ec16c99f3067bf04
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-23 06:33:17 -0400

    remove apple script implementation in favor of ruby implementation.

---
+++ desktop_background_changer.rb
@@ -0,0 +1,4 @@
+  folder = "/System/Library/Frameworks/ScreenSaver.framework/Resources"
+    Dir.chdir("#{folder}") do |i|
+      `./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background`
+    end

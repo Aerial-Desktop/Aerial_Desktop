fd15f82e99598d69cb50bf8372e1d1a188c7a19b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-01 19:25:28 -0400

    simplify application layout;

--- install_driver.sh
+++ install_driver.sh
@@ -2,16 +2,16 @@
 
 # write out path needed
 pwd=$PWD
-pwd2="              <string>$pwd/./DesktopBackgroundChanger.app/Contents/MacOS/DesktopBackgroundChanger</string>"
-echo $pwd2 
+pwd2="              <string>$pwd/app/./DesktopBackgroundChanger.app/Contents/MacOS/DesktopBackgroundChanger</string>"
+echo $pwd2
 
 # insert path needed into correct file.
 # line 9 in com.example.hello.plist
-sed -i '' '9s?.*?'"$pwd2"'?' com.example.hello.plist
+sed -i '' '9s?.*?'"$pwd2"'?' app/com.example.hello.plist
 
 # change environment appropriately 
- chmod 644 com.example.hello.plist
+ chmod 644 app/com.example.hello.plist
 
- cp com.example.hello.plist ~/Library/LaunchAgents/
+ cp app/com.example.hello.plist ~/Library/LaunchAgents/
 
  launchctl load ~/Library/LaunchAgents/com.example.hello.plist

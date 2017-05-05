b719adab12e6058505fd2aa943fc52e6f2621851
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-05 12:37:23 -0400

    modified script away from pwd to location of script being run.

--- app/install_driver.sh
+++ app/install_driver.sh
@@ -1,16 +1,42 @@
 # !/bin/bash
 
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+echo "DIR is '$DIR'"
+
+#current_dir=$(pwd)
+#script_dir=$(dirname $0)
+
+#echo $current_dir
+#echo $script_dir
+
 # write out path needed
-pwd=$PWD
-pwd2="              <string>$pwd/./app/DesktopBackgroundChanger.app/Contents/Resources/script</string>"
+#pwd=$PWD
+pwd2="              <string>$DIR/./DesktopBackgroundChanger.app/Contents/Resources/script</string>"
 echo $pwd2
 # insert path needed into correct file.
 # line 9 in com.example.hello.plist
-sed -i '' '9s?.*?'"$pwd2"'?' app/com.example.hello.plist
+sed -i '' '9s?.*?'"$pwd2"'?' $DIR/com.example.hello.plist
 
-# change environment appropriately 
- chmod 644 app/com.example.hello.plist
+# change environment appropriately
+ chmod 644 $DIR/com.example.hello.plist
 
- cp app/com.example.hello.plist ~/Library/LaunchAgents/
+ cp $DIR/com.example.hello.plist ~/Library/LaunchAgents/
 
  launchctl load ~/Library/LaunchAgents/com.example.hello.plist

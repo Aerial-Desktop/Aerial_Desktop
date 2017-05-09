4ec03810ab9d05ea4238f2c9716f9ddcd243e712
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-09 01:28:09 -0400

    change to one line of code stackoverflow implementation;
    http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within

--- app/install_launch_agent.sh
+++ app/install_launch_agent.sh
@@ -1,23 +1,7 @@
 # !/bin/bash
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  TARGET="$(readlink "$SOURCE")"
-  if [[ $TARGET == /* ]]; then
-    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
-    SOURCE="$TARGET"
-  else
-    DIR="$( dirname "$SOURCE" )"
-    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
-    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-  fi
-done
-echo "SOURCE is '$SOURCE'"
-RDIR="$( dirname "$SOURCE" )"
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-if [ "$DIR" != "$RDIR" ]; then
-  echo "DIR '$RDIR' resolves to '$DIR'"
-fi
-echo "DIR is '$DIR'"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 
 # write out path needed
 # pwd=$PWD
@@ -25,7 +9,7 @@ pwd2="              <string>$DIR/./DesktopBackgroundChanger.app/Contents/Resourc
 echo $pwd2
 # insert path needed into correct file.
 # line 9 in com.example.hello.plist
-sed -i '' '9s?.*?'"$pwd2"'?' $pwd/com.example.hello.plist
+sed -i '' '9s?.*?'"$pwd2"'?' $DIR/com.example.hello.plist
 
 # change environment appropriately
 chmod 644 $DIR/com.example.hello.plist

83a3fff1d34761bb982d95dd2ffaa1b44bd02428
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-09 01:22:30 -0400

    change script from current pwd to own script location.
    Making more reliable and scalable script.
    Credit goes to these guy, getting a script to know its own location:
    http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within

--- app/install_launch_agent.sh
+++ app/install_launch_agent.sh
@@ -1,10 +1,28 @@
 # !/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+echo "DIR is '$DIR'"
 
 # write out path needed
 # pwd=$PWD
 pwd2="              <string>$DIR/./DesktopBackgroundChanger.app/Contents/Resources/script</string>"
 echo $pwd2
-
 # insert path needed into correct file.
 # line 9 in com.example.hello.plist
 sed -i '' '9s?.*?'"$pwd2"'?' $pwd/com.example.hello.plist

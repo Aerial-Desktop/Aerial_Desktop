222e23d8b987b7fe856aeaa68ac20d3d4e5c7909
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-05-17 11:25:29 -0400

    add workaround fix for local install https://github.com/JohnCoates/Aerial/issues/4

---
+++ app/local_install_workaround.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+if [ -d ~/Library/Screen\ Savers/Aerial.saver ] ; then
+  defaults -currentHost write com.apple.screensaver modulePath -string "/System/Library/Screen Savers/Shell.saver"; defaults -currentHost write com.apple.screensaver moduleName -string "Shell"; defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Shell path /System/Library/Screen\ Savers/Shell.saver/ type 0;
+  defaults -currentHost write com.apple.screensaver modulePath -string "~/Library/Screen\ Savers/Aerial.saver"; defaults -currentHost write com.apple.screensaver moduleName -string "Aerial";defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Aerial path ~/Library/Screen\ Savers/Aerial.saver type 0;
+  osascript -e 'display notification "Installed for this user only, enjoy." with title "Success, Aerial Desktop"'
+elif [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
+  defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Aerial path /Library/Screen\ Savers/Aerial.saver type 0
+  osascript -e 'display notification "Installed for all users, enjoy." with title "Success, Aerial Desktop"'
+else 
+  osascript -e 'display notification "Did you put your credentials? Uninstall and reinstall, thanks." with title "error 333 Aerial Desktop"'
+fi

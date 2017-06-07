8ad9c0c2a950b1aeecebf71c176bd09afdcecbee
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-06-07 01:50:49 -0400

    improve user experience by changin folder layout

--- .hide/app/install/1_download_aerial.sh
+++ .hide/app/install/1_download_aerial.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
     echo "File exists."
   else
-    open $DIR/../bin/Aerial.saver/
+    open $DIR/../../bin/Aerial.saver/
     sleep 5
     osascript -e 'display notification "To allow for User decision/credentials" with title "Application Waiting ten seconds"'
     sleep 5

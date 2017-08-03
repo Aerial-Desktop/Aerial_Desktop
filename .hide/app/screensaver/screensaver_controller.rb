5fd3fe91801dbdbcad94e7ff61451b6ec55f4998
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-03 16:20:24 -0400

    screensaver_controller rubify

--- .hide/app/screensaver/screensaver_controller.rb
+++ .hide/app/screensaver/screensaver_controller.rb
@@ -1,10 +1,10 @@
 # !/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# will have require_relative '../check/check_module'
+`DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
 
 # run aerial application
   if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
-    echo "File exists."
+    `echo "File exists."`
   else
-    open $DIR/../../bin/Aerial.saver/
-  fi
+    `open $DIR/../../bin/Aerial.saver/`
+  end

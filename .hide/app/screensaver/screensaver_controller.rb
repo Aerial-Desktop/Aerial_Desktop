8d195fbb9dc7b8a10eca66a59d014226f1dfdb0d
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-03 17:14:27 -0400

    screensaver_controller rubify, fix if statements.

--- .hide/app/screensaver/screensaver_controller.rb
+++ .hide/app/screensaver/screensaver_controller.rb
@@ -3,8 +3,8 @@
 `DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
 
 # run aerial application
-  if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
-    `echo "File exists."`
-  else
-    `open $DIR/../../bin/Aerial.saver/`
-  end
+#  if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
+#    `echo "File exists."`
+#  else
+#    `open $DIR/../../bin/Aerial.saver/`
+#  end

4b4e3a156d3b83071840d16ad9c830f00b61798d
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-03 17:12:54 -0400

    notification rubify... minimum still in want of big changes.

--- .hide/app/notifications/uninstall_notification_controller.rb
+++ .hide/app/notifications/uninstall_notification_controller.rb
@@ -1,12 +1,12 @@
-if [ -d ~/Library/Screen\ Savers/Aerial.saver ] ; then
+`if [ -d ~/Library/Screen\ Savers/Aerial.saver ] ; then
   rm -rf ~/Library/Screen\ Savers/Aerial.saver/
   osascript -e 'display notification "Aerial Screensaver has been removed." with title "Aerial Desktop Uninstal Successful"'
 elif [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
-  #build notes: will need to open a popup window and gather input 
+  #build notes: will need to open a popup window and gather input
   #then put it as the pasword information for sudo
 osascript -e 'display notification "Did not Uninstall currently in dev,\nReason: installed across all Users; need sudo"'
   sudo rm -rf /Library/Screen\ Savers/Aerial.saver/
 else
   osascript -e 'display notification "File does not exist." with title "Notification: Aerial Desktop"'
 fi
-  defaults -currentHost write com.apple.screensaver modulePath -string "/System/Library/Screen Savers/Shell.saver"; defaults -currentHost write com.apple.screensaver moduleName -string "Shell"; defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Shell path /System/Library/Screen\ Savers/Shell.saver/ type 0;
+  defaults -currentHost write com.apple.screensaver modulePath -string "/System/Library/Screen Savers/Shell.saver"; defaults -currentHost write com.apple.screensaver moduleName -string "Shell"; defaults -currentHost write com.apple.screensaver moduleDict -dict moduleName Shell path /System/Library/Screen\ Savers/Shell.saver/ type 0;`

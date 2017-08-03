fd15c1248f9a44049826a0f0640dd163b9151513
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-03 17:21:49 -0400

    Start Stop Install Uninstall begin rubify install_controller.rb

--- .hide/app/install/install_controller.rb
+++ .hide/app/install/install_controller.rb
@@ -1,15 +1,12 @@
-#!/bin/bash
-adaptive_wait () {
-TIMEOUT=25
-  until [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] || [ $TIMEOUT -eq 0 ] ; do
-    if ! ((TIMEOUT % 5)); then
-    osascript -e 'display notification "To allow for Decision/User Credentials" with title "Application Waiting"'
-    fi
-    sleep 1
-    TIMEOUT=$[$TIMEOUT-1]
-  done
-}
-./.hide/app/screensaver/screensaver_controller.rb
-adaptive_wait
-./.hide/app/notification/notification_controller.rb
-./.hide/app/launch_agent/launch_agent_controller.rb
+require_relative ../screensaver/screensaver_controller.rb
+require_relative ../notifications/notification_controller.rb
+require_relative ../launch_agent/launch_agent_controller.rb
+require_relative ../check/check_controller.rb
+class InstallController
+  # to do... everything should be refactored into module's
+  # the state for this application is the macintosh environment.
+  = ScreensaverController.new
+  = CheckController.new
+  = NotificationController.new
+  = LaunchAgentController.new
+end

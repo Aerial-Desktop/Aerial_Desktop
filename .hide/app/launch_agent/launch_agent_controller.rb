dd135f7d8a13114241fd5ed06f419dd12353be36
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-05 18:38:26 -0400

    to be reset back

--- .hide/app/launch_agent/launch_agent_controller.rb
+++ .hide/app/launch_agent/launch_agent_controller.rb
@@ -3,27 +3,5 @@
 # require relative .tmp
 
 # write out path needed
-`DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LA=$DIR
-SE=$DIR/../../bin
-
-echo $DIR
-
-
-#create .tmp file
-mkdir $LA/.tmp
-cp $LA/com.example.hello.plist $LA/.tmp/com.example.hello.plist
-
-# create the string needed.
-pwd2="              <string>$SE/./screensaver_engine.app/Contents/Resources/script</string>"
-echo $pwd2
-# insert path needed into correct file.
-# line 9 in com.example.hello.plist
-sed -i '' '9s?.*?'"$pwd2"'?' $LA/.tmp/com.example.hello.plist
-
-# change environment appropriately
-chmod 644 $LA/.tmp/com.example.hello.plist
-
-cp $LA/.tmp/com.example.hello.plist ~/Library/LaunchAgents/
-
-launchctl load ~/Library/LaunchAgents/com.example.hello.plist`
+class LaunchAgentController
+end
--- .hide/app/screensaver/screensaver_controller.rb
+++ .hide/app/screensaver/screensaver_controller.rb
@@ -1,10 +1,4 @@
 # !/bin/bash
 # will have require_relative '../check/check_module'
-`DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"`
-
-# run aerial application
-#  if [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] ; then
-#    `echo "File exists."`
-#  else
-#    `open $DIR/../../bin/Aerial.saver/`
-#  end
+class ScreensaverController
+end

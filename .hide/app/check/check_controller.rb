77d21134327c8837e2baa16cf6fd075a44a49076
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-05 18:37:36 -0400

    check, add check controller.rb

--- .hide/app/check/check_controller.rb
+++ .hide/app/check/check_controller.rb
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-`adaptive_wait () {
-TIMEOUT=25
-  until [ -d ~/Library/Screen\ Savers/Aerial.saver ] || [ -d /Library/Screen\ Savers/Aerial.saver ] || [ $TIMEOUT -eq 0 ] ; do
-    if ! ((TIMEOUT % 5)); then
-    osascript -e 'display notification "To allow for Decision/User Credentials" with title "Application Waiting"'
-    fi
-    sleep 1
-    TIMEOUT=$[$TIMEOUT-1]
-  done
-}`
+class CheckController
+end
 
 

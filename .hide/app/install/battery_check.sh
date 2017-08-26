cbb6f3d84228f3c364e9014f5a837d3958b5e8e8
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-08-26 12:13:54 -0400

    welcome to class, intro to how to write bad bash script.
    add really bad code to compare battery percentage in bash.

--- .hide/app/install/battery_check.sh
+++ .hide/app/install/battery_check.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+# find out if charging 
 result=$(ioreg -l | grep IsCharging | cut -d " " -f 19;)
 echo $result
+# end find out if charging 
+
+# find out battery percentage
+get_percentage(){ numerator=$(ioreg -n AppleSmartBattery | grep CurrentCapacity | cut -d " " -f 19;);denominator=$(ioreg -n AppleSmartBattery | grep MaxCapacity | cut -d " " -f 19;); echo $"(($numerator/$denominator))" | bc -l | while read tmp_percentage; do percentage=${tmp_percentage}; echo "$percentage"; done }
+# end find battery percentage
+
+# check battery to acceptance criteria.
+percentage="$(get_percentage)";
+test=.20;
+
+if (( $(echo "$percentage > $test" | bc -l) )) ; then
+  echo "$percentage greater than .20"
+elif (( $(echo "$percentage < $test" | bc -l) )) ; then
+  echo "$percentage less than .20"
+else 
+  echo "nothing found"
+fi
+# end check battery to acceptance criteria.

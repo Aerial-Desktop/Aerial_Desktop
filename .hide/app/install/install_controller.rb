db1741a010f3c03b38cf7105f91e60abcca1be3c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-09-03 12:55:31 -0400

    I like the way it reads, Hopefull 🤓 🤞

--- .hide/app/install/install_controller.rb
+++ .hide/app/install/install_controller.rb
@@ -5,8 +5,20 @@ require_relative ../check/check_controller.rb
 class InstallController
   # to do... everything should be refactored into module's
   # the state for this application is the macintosh environment.
-  = ScreensaverController.new
-  = CheckController.new
-  = NotificationController.new
-  = LaunchAgentController.new
+
+  #story, how this application works.
+  #users interact with the menubar... that is an interface or dashboard or something.
+  #the install requests come into this file.
+  #
+  #this file handles
+  # 1.Installing the particular screensaver
+  # 2.probably monitoring the status ... or telling the monitor to do stuff.
+  # 2.changing the mac_defaults.
+  # 3.telling the launch agent to run.
+  # 4.allowing users to customize the stuff later.
+  #
+  # 5.really this should be manager. not install.
+  # 5.manager can, install, start, stop, and uninstall.
+  #
+  # 6.the interface is manipulated by the User and the manager takes care of it
 end

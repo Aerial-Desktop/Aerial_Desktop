f1ffc7a8d06650cf78e033ae20655825431af8b1
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-09-03 06:53:20 -0400

    scalable variable names work.

--- .hide/app/install/1_download_aerial.sh
+++ .hide/app/install/1_download_aerial.sh
@@ -3,7 +3,7 @@
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   cd $DIR;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/Epoch%20Flip%20Clock.saver;
-  #svn checkout https://github.com/MichaelDimmitt/sc/trunk/Aerial.saver;
+  svn checkout https://github.com/MichaelDimmitt/sc/trunk/Aerial.saver;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/Blue Screen Saver.saver;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/FractalClock.saver;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/KPSaver.saver;
@@ -11,9 +11,9 @@
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/MatrixScreenSaver.saver;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/StarWarsScroll.saver;
   #svn checkout https://github.com/MichaelDimmitt/sc/trunk/WhatColourIsIt.saver;
-  svn checkout https://github.com/MichaelDimmitt/sc/trunk/Word%20Clock.saver;
+#  svn checkout https://github.com/MichaelDimmitt/sc/trunk/Word%20Clock.saver;
 # screen_saver=Epoch\ Flip\ Clock.saver;
-# screen_saver=Aerial.saver;
+ screen_saver=Aerial.saver;
 # screen_saver=Blue Screen Saver.saver;
 # screen_saver=FractalClock.saver;
 # screen_saver=KPSaver.saver;
@@ -21,7 +21,7 @@
 # screen_saver=MatrixScreenSaver.saver;
 # screen_saver=StarWarsScroll.saver;
 # screen_saver=WhatColourIsIt.saver;
-  screen_saver=Word\ Clock.saver/
+  #screen_saver=Word\ Clock.saver/
   say $DIR/$screen_saver   
   mv $DIR/$screen_saver $DIR/../../bin/$screen_saver
   # mv Aerial.saver $DIR/../../bin/Aerial.saver;
@@ -32,6 +32,6 @@
   else
     # svn checkout https://github.com/MichaelDimmitt/sc/trunk/KPSaver.saver;
     #open $DIR/../../bin/Aerial.saver/
-    open $DIR/../../bin/Word\ Clock.saver/
-    #open $DIR/../../bin/$screen_saver/
+    #`open $DIR/../../bin/Word\ Clock.saver/
+    open $DIR/../../bin/$screen_saver/
   fi

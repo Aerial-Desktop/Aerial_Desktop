921752a8a9874fb8b75ad1efcc97e930f421b043
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-09-04 17:47:33 -0400

    possible, refactor: let the program build string
    what we are doing is we are coupling.
    
    if a bad change happens it will show and fixing will be easier.
    general improvements will also be easier. 
    
    this does not work well if extensive customization in between.

--- .hide/app/dumb_components/notifications/notification_controller.rb
+++ .hide/app/dumb_components/notifications/notification_controller.rb
@@ -1,13 +1,22 @@
 # !/bin/bash
 
 module dumb_notifications
-
+  
   def installed_locally
-    osascript -e 'display notification "Installed for this user only, enjoy." with title "Success, Aerial Desktop"'
+    have_a_third_method_that_builds_it("this user")
+  end
+
+  def installed_across_all_users
+    have_a_third_method_that_builds_it("all users")
+  end
+  
+  def have_a_third_method_that_builds_it(users)
+    osascript -e 'display notification "Installed for #{users} only, enjoy." with title "Success, Aerial Desktop"'
   end
 
   def installed_across_all_users
     osascript -e 'display notification "Installed for all users, enjoy." with title "Success, Aerial Desktop"'
+    have_a_third_method_that_builds_it("all users")
   end
 
   def prompt_or_credential_input_time_expired

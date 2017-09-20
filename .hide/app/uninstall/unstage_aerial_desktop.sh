879da6ffeef372f8ae66a857b65c428d94968c0b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-09-19 23:20:36 -0400

    give launch agents unique names ✌️

--- .hide/app/uninstall/unstage_aerial_desktop.sh
+++ .hide/app/uninstall/unstage_aerial_desktop.sh
@@ -2,4 +2,6 @@
 
  launchctl unload ~/Library/LaunchAgents/com.example.hello.plist;
  rm ~/Library/LaunchAgents/com.example.hello.plist
- # mv ~/Library/LaunchAgents/com.example.hello.plist com.example.hello.plist;
+ launchctl unload ~/Library/LaunchAgents/com.aerial.desktop.plist;
+ rm ~/Library/LaunchAgents/com.aerial.desktop.plist
+ # mv ~/Library/LaunchAgents/com.aerial.desktop.plist com.aerial.desktop.plist;
--- .hide/app/uninstall/unstage_launch_agent.sh
+++ .hide/app/uninstall/unstage_launch_agent.sh
@@ -3,6 +3,8 @@ osascript -e 'quit app "System Preferences"'
 
  launchctl unload ~/Library/LaunchAgents/com.example.hello.plist;
  rm ~/Library/LaunchAgents/com.example.hello.plist
- launchctl unload ~/Library/LaunchAgents/com.example.charg.plist;
- rm ~/Library/LaunchAgents/com.example.charg.plist
- # mv ~/Library/LaunchAgents/com.example.hello.plist com.example.hello.plist;
+ launchctl unload ~/Library/LaunchAgents/com.aerial.desktop.plist;
+ rm ~/Library/LaunchAgents/com.aerial.desktop.plist
+ launchctl unload ~/Library/LaunchAgents/com.monitor.charge.plist;
+ rm ~/Library/LaunchAgents/com.monitor.charge.plist
+ # mv ~/Library/LaunchAgents/com.aerial.desktop.plist com.aerial.desktop.plist;

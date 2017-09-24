b2f04f034eab0237e8cba30e9e8e4cabcd613f2c
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-09-24 09:35:05 -0400

    battery percentage and charge threshold working

--- .hide/app/install/battery_check.sh
+++ .hide/app/install/battery_check.sh
@@ -11,12 +11,17 @@ get_percentage(){ numerator=$(ioreg -n AppleSmartBattery | grep CurrentCapacity 
 # check battery to acceptance criteria.
 percentage="$(get_percentage)";
 battery_threshold=.40;
-charger_threshold=.20
+charger_threshold=.20;
 
+if (( $(echo "$percentage < $charger_threshold" | bc -l) )) ; then
+  echo "$percentage greater than charger threshold $charger_threshold"
+elif (( $(echo "$percentage < $charger_threshold" | bc -l) )) ; then
+  echo "$percentage less than charger threshold$charger_threshold"
+fi
 if (( $(echo "$percentage > $battery_threshold" | bc -l) )) ; then
-  echo "$percentage greater than $battery_threshold"
+  echo "$percentage greater than battery threshold $battery_threshold"
 elif (( $(echo "$percentage < $battery_threshold" | bc -l) )) ; then
-  echo "$percentage less than $battery_threshold"
+  echo "$percentage less than battery threshold $battery_threshold"
 else 
   echo "nothing found"
 fi
@@ -31,14 +36,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ "$result" == "Yes" ] ; then
   # echo Charging;
   # osascript -e 'display notification "Since Computer is Charging, program ok." with title "Aerial Desktop"'
+#creation action
   if (( $(echo "$percentage < $charger_threshold" | bc -l) )) ; then
+    $DIR/./../uninstall/unstage_aerial_desktop.sh ;
+  else 
     $DIR/./3_install_launch_agent.sh
   fi
 elif [ "$result" == "No" ] ; then
   echo Not Charging;
+#deletion action
   if (( $(echo "$percentage < $battery_threshold" | bc -l) )) ; then
     # echo not charging and battery unacceptable level.
     # osascript -e 'display notification "Computer not charging and low battery program disabled." with title "Aerial Desktop"'
+#deletion action has a different sign than a cration action
+#namely switching > to <
     $DIR/./../uninstall/unstage_aerial_desktop.sh
   else
     # echo but at an acceptable battery level.

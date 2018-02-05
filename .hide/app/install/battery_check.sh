e332c6f3af2fc58ea789611fe60feddfd1f44b46
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-02-05 04:00:20 -0500

    #19, internet checking solution looks as if it is working.

--- .hide/app/install/battery_check.sh
+++ .hide/app/install/battery_check.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# gather information to be able to run scripts from this file location.
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# say $DIR
+# end gather information.
+
 # find out if charging 
 result=$(ioreg -l | grep IsCharging | cut -d " " -f 19;)
 echo $result
@@ -21,16 +27,16 @@ else
 fi
 # end check battery to acceptance criteria.
 
-# gather information to be able to run scripts from this file location.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# say $DIR
-# end gather information.
-
 # perform actions based on computer's charged state.
 if [ "$result" == "Yes" ] ; then
   # echo Charging;
+  if curl --silent --head https://github.com/michaeldimmitt/aerial_desktop;  
+    then $DIR/./3_install_launch_agent.sh# echo Internet status: OK; 
+    else osascript -e 'display notification "Please connect your computer to internet." with title "Aerial Desktop"'# echo Internet status: ERROR; 
+  fi | tail -n 1
+
   # osascript -e 'display notification "Since Computer is Charging, program ok." with title "Aerial Desktop"'
-  $DIR/./3_install_launch_agent.sh
+  
 elif [ "$result" == "No" ] ; then
   echo Not Charging;
   if (( $(echo "$percentage < $battery_threshold" | bc -l) )) ; then
@@ -40,7 +46,10 @@ elif [ "$result" == "No" ] ; then
   else
     # echo but at an acceptable battery level.
     # osascript -e 'display notification "Computer not charging and but at an acceptable battery level, program ok." with title "Aerial Desktop"'
-    $DIR/./3_install_launch_agent.sh
+    if curl --silent --head https://github.com/michaeldimmitt/aerial_desktop;  
+      then $DIR/./3_install_launch_agent.sh# echo Internet status: OK; 
+      else osascript -e 'display notification "Please connect your computer to internet." with title "Aerial Desktop"'# echo Internet status: ERROR; 
+    fi | tail -n 1
   fi
 else 
   osascript -e 'display notification " something went wrong, contact michael; did some bad programming." with title "Aerial Desktop"'

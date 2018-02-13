b31fb90e7631a2320d777afd11f65c9fb398d2a1
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-02-12 23:57:17 -0500

    push up for comparison.

--- .hide/app/uninstall/2_unstage_launch_agent.sh
+++ .hide/app/uninstall/2_unstage_launch_agent.sh
@@ -1,10 +1,13 @@
 # !/bin/bash
 osascript -e 'quit app "System Preferences"'
 
+ launchctl unload ~/Library/LaunchAgents/com.desktop.dyna.plist;
+ rm ~/Library/LaunchAgents/com.desktop.dyna.plist
+ launchctl unload ~/Library/LaunchAgents/com.monitor.charge.plist;
+ rm ~/Library/LaunchAgents/com.monitor.charge.plist 
+ 
+ # in case people have the old launch agents 😐
  launchctl unload ~/Library/LaunchAgents/com.example.hello.plist;
  rm ~/Library/LaunchAgents/com.example.hello.plist
  launchctl unload ~/Library/LaunchAgents/com.aerial.desktop.plist;
  rm ~/Library/LaunchAgents/com.aerial.desktop.plist
- launchctl unload ~/Library/LaunchAgents/com.monitor.charge.plist;
- rm ~/Library/LaunchAgents/com.monitor.charge.plist
- # mv ~/Library/LaunchAgents/com.aerial.desktop.plist com.aerial.desktop.plist;

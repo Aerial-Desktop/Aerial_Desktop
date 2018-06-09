b289fb7b177d25b6c2a14a9fef8cf7c55d117a45
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-06-08 22:09:51 -0400

    simplify code seperating duplicate internet connection logic.

--- .hide/app/install/4_desktop_background_changer.sh
+++ .hide/app/install/4_desktop_background_changer.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # test_date() uninstall launch_agent if computer has slept twice quickly...
 # handling potential high sierra catestrophy
-check_launch_agent () {
+check_launch_agent() {
   if [ "$(launchctl list | grep aerial)" -ge 1 ];
   then 
     osascript -e 'display notification "Please connect your computer to internet." with title "Aerial Desktop"';
@@ -17,37 +17,38 @@ check_launch_agent () {
   fi
 }
 
+check_internet_connection() {
+  if curl --silent --head https://github.com/michaeldimmitt/aerial_desktop;
+    then 
+      sleep 3;
+      `/System/Library/Frameworks/ScreenSaver.framework/Resources/./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background`;
+      # echo Internet status: OK; 
+    else 
+      check_launch_agent
+      # echo Internet status: ERROR; 
+  fi | tail -n 1 
+}
+
 test_date() {
   num_lines=$(cat $DIR/.timestamp.txt | wc -l)
   case $(echo $num_lines) in
     [0-1])
-        date +%s >> $DIR/.timestamp.txt
-        if curl --silent --head https://github.com/michaeldimmitt/aerial_desktop;
-          then 
-            sleep 3;
-            `/System/Library/Frameworks/ScreenSaver.framework/Resources/./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background`;# echo Internet status: OK; 
-          else 
-            check_launch_agent
-        fi | tail -n 1 
+        # write date timestamp
+        date +%s >> $DIR/.timestamp.txt;
+        # check the internet
+        check_internet_connection;
         ;;
     2)
       old_time=`cat $DIR/.timestamp.txt`;
+      # subtract current time from the second time on the timestamp to get a count.
       count=`expr $(date +%s) - $(sed -n '2p' $DIR/.timestamp.txt)`;
-      if [ "$count" -le 100 ]; then
+      if [ "$count" -le 100 ]; then # less than two minutes.
         osascript -e 'display notification "Uninstalled because you put your computer to sleep twice really quickly.😍✌️" with title "Uninstalled due rapid sleep, please reinstall."'
         ./$DIR/../uninstall/2_unstage_launch_agent.sh
       else
-        sed -i '' '1d' $DIR/.timestamp.txt;
+        # sed -i '' '1d' $DIR/.timestamp.txt;
         date +%s >> $DIR/.timestamp.txt;
-        if curl --silent --head https://github.com/michaeldimmitt/aerial_desktop;
-          then 
-            sleep 3;
-            `/System/Library/Frameworks/ScreenSaver.framework/Resources/./ScreenSaverEngine.app/Contents/MacOS/ScreenSaverEngine -background`;
-            # echo Internet status: OK; 
-          else 
-            check_launch_agent
-            # echo Internet status: ERROR; 
-        fi | tail -n 1 
+        check_internet_connection;
       fi
       ;;
     *)
